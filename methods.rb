706f6b0d49f6ca21100a67432117145801a9f861
AngieMwaniki <angela.mwaniki@student.moringaschool.com>
2022-10-24 06:22:39 +0300

    completed

--- methods.rb
+++ methods.rb
@@ -1,1 +1,30 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+greet ("Naureen")
+
+def greet_with_default (name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+
+
+def add(num1,num2)
+    puts num1, num2
+    return num1+num2
+end
+add(10,10)
+
+
+
+def halve(number)
+    if number.class != Integer
+        return nil
+    end
+   number/2
+end
